56303b2de5d155957f0ad813aa153ef0e57e4235
Nikhil <nikhilcoder061@gmail.com>
2025-03-10 12:28:19 +0530

    file system

---
+++ 5 MERN/Backend/fs/index.js
@@ -0,0 +1,31 @@
+// file system - fs module
+// Node JS
+
+const fs = require('fs');
+
+// fs.readFile('test.txt', 'utf-8', (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log(result);
+//     }
+// })
+
+// const result = fs.readFileSync('test.txt', 'utf-8');
+
+// console.log(result);
+
+// fs.appendFileSync('test.txt', ' wscubtech') // 
+// fs.cpSync('test.txt', 'abc.txt')
+// fs.unlinkSync('test.txt')
+// fs.writeFileSync('test.txt', 'hello how are you');
+
+// const result = fs.statSync('test.txt');
+// console.log(result);
+// fs.appendFileSync('test.txt', 'wscubetech')
+
+// const result =fs.statSync('test.txt');
+
+// console.log(result);
+
+// fs.mkdirSync('demo1');
