44668d8bdc0302a7b6c5be13694cf65cc2ce0c3c
Nikhil <nikhilcoder061@gmail.com>
2025-03-17 16:03:18 +0530

    register,read, login

--- 5 MERN/Backend/express/index.js
+++ 5 MERN/Backend/express/index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 
 // create a server start  
 const server = express();
+server.use(express.json()); // middleware
 
 // create a server end
 
@@ -32,7 +33,8 @@ const userData = mongoose.Schema(
         password: {
             type: String,
             minLength: 8,
-            maxLength: 15
+            maxLength: 15,
+            required: true
         },
         status: {
             type: Boolean,
@@ -46,12 +48,139 @@ const userData = mongoose.Schema(
 const userModel = mongoose.model("users", userData);// collection name, schema varible
 // create a collection of user in mongodb end
 
+
+// create a user start
 server.post(
     "/user/register",
     (req, res) => {
+        try {
+            const user = new userModel(
+                {
+                    name: req.body.name,
+                    email: req.body.email,
+                    phone: req.body.phone,
+                    age: req.body.age,
+                    password: req.body.password
+                }
+            )
+
+            user.save().then(
+                (success) => {
+                    res.send(
+                        {
+                            msg: "User created successfully",
+                            status: 1
+                        }
+                    )
+                }
+            ).catch(
+                (error) => {
+                    console.log(error);
+                    res.send(
+                        {
+                            msg: "User not created",
+                            status: 0
+                        }
+                    )
+                }
+            )
+        } catch (error) {
+            console.log(error);
+            res.send(
+                {
+                    msg: "Internal Server Error",
+                    status: 0
+                }
+            )
+        }
+
+
+
 
     }
 ) // path, function
+// create a user end
+
+//login user start
+server.post(
+    "/user/login",
+    async (req, res) => {
+        try {
+            const user = await userModel.findOne(
+                {
+                    email: req.body.email
+                }
+            )
+
+            if (user) {
+                if (user.password == req.body.password) {
+                    res.send(
+                        {
+                            msg: "Login Successfully",
+                            status: 1
+                        }
+                    )
+                } else {
+                    res.send(
+                        {
+                            msg: "Password incorrect",
+                            status: 0
+                        }
+                    )
+                }
+            } else {
+                res.send(
+                    {
+                        msg: "User not Found",
+                        status: 0
+                    }
+                )
+            }
+
+        } catch (error) {
+            console.log(error);
+            res.send(
+                {
+                    msg: "Internal Server error",
+                    status: 0
+                }
+            )
+        }
+    }
+)
+//login user end
+
+// read users start
+server.get(
+    "/user",
+    async (req, res) => {
+        try {
+
+            const users = await userModel.find();
+            if (users) {
+                res.send(
+                    {
+                        msg: "Users Found",
+                        status: 1,
+                        users
+                    }
+                )
+            } else {
+                res.send(
+                    {
+                        msg: "Users not found",
+                        status: 0
+                    }
+                )
+            }
+
+        } catch (error) {
+
+        }
+    }
+)
+// read users end
+
 
 
 
